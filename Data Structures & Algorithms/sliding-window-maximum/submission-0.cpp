efa657a0f4f4f0a458039385fd57af7a8b457b7e
Gaurav Kumar Choudhary <gauravgkchy139@gmail.com>
2026-07-28 14:27:02 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        int i = 0;
+        for(int j = 0; j < nums.size(); j++) {
+            mp[nums[j]]++;
+            while(j-i+1 > k) {
+                mp[nums[i]]--;
+                if(mp[nums[i]] == 0) mp.erase(nums[i]);
+                i++;
+            }
+            if(j-i+1 == k) {
+                int maxi = INT_MIN;
+                for(auto &p: mp) {
+                    maxi = max(maxi,p.first);
+                }
+                ans.push_back(maxi);
+            }
+        }
+        return ans;
+    }
+};
