6066247d253900da03cf9c1eb41dcd69e2f11fdb
Gaurav Kumar Choudhary <gauravgkchy139@gmail.com>
2026-07-29 17:25:00 +0530

    Add: car-fleet - submission-8

---
+++ Data Structures & Algorithms/car-fleet/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        stack<float> st;
+        unordered_map<int,int> mp;
+        for(int i = 0; i < n; i++) {
+            mp[position[i]] = i;
+        }
+        sort(position.rbegin(),position.rend());
+
+        for(int i = 0; i < n; i++) {
+            float time = (target-position[i])/(1.0*speed[mp[position[i]]]);
+            if(st.empty() || time > st.top()) {
+                st.push(time);
+            }
+            
+        }
+        return st.size();
+    }
+};
